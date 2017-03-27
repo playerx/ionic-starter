3d4728ef987abec51916cc1fadc190e11a6b2d27
Ezeki EZ. Zibzibadze <e.zibzibadze@altasoft.local>
2017-03-27 15:44:24 +0400

    added cordova

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { HomePage } from 'pages';
+import { Platform, AlertController } from 'ionic-angular'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.ts.html',
-  styleUrls: ['app.component.ts.css']
+	selector: 'app-root',
+	templateUrl: 'app.component.ts.html',
+	styleUrls: ['app.component.ts.css']
 })
 export class AppComponent {
-  title = 'app works!';
+	title = 'app works!';
 
-  rootPage = HomePage
+	rootPage = HomePage
+
+
+	constructor(private platform: Platform, private alert: AlertController) {
+
+	}
+
+	async ngOnInit() {
+
+		this.alert.create({
+			title: 'Platform is not ready yet',
+			buttons: ['Okey']
+		}).present();
+
+		await this.platform.ready();
+
+
+		this.alert.create({
+			title: 'Platform is Ready!',
+			buttons: ['Cool']
+		}).present();
+	}
 }
